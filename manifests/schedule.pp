38c6a70693d7851a7d8090712f862fd9275929c6
Bren Briggs <briggs.brenton@gmail.com>
2015-12-17 11:26:17 -0500

    Fix lint issues

--- manifests/schedule.pp
+++ manifests/schedule.pp
@@ -1,16 +1,16 @@
 define catfacts::schedule (
     $target,
     $sender,
-    $user = 'root', 
+    $user = 'root',
     $minute = '0',
     $hour = '*',
 ) {
         include catfacts
         include catfacts::params
-        cron {"${name}":
+        cron {$name:
             command => "${catfacts::params::fortune_location} /etc/catfacts/catfacts -s -n 140 | ${catfacts::params::mailer_location} -f ${sender} ${target}",
-            minute  => "${minute}",
-            hour    => "${hour}",
-            user    => "${user}"
+            minute  => $minute,
+            hour    => $hour,
+            user    => $user
     }
 }
