737eb2a5cd03dadc3fa63552cbbc224c912d4760
Bren Briggs <briggs.brenton@gmail.com>
2016-02-08 20:51:32 -0500

    Add testing suite

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
-describe 'catfacts' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('catfacts') }
-  end
+describe 'catfacts', :type => :class do
+
+    context 'Debian with default params' do
+        let(:facts) { {
+          :osfamily => 'Debian',
+        } }
+        it { should create_class('catfacts') }
+        it { should create_file('/etc/catfacts/catfacts') }
+        it { should create_file('etc/catfacts/catfacts.dat') }
+        it { should create_package('fortune') }
+    end
+
+    context 'RedHat with default params' do
+        let(:facts) { {
+          :osfamily => 'RedHat',
+        } }
+        it { should create_class('catfacts') }
+        it { should create_file('/etc/catfacts/catfacts') }
+        it { should create_file('etc/catfacts/catfacts.dat') }
+        it { should create_package('fortune') }
+    end
+
+    context 'Unsupported with default params' do
+        let(:facts) { {
+          :osfamily => 'BSD',
+        } }
+        it { is_expected.to raise_error(Puppet::Error) }
+    end
 end
