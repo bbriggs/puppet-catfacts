b33c683485b514c086f52c653dbbde990b4d82c1
Bren Briggs <briggs.brenton@gmail.com>
2016-02-13 10:29:49 -0500

    Fixing linter complaints

--- manifests/schedule.pp
+++ manifests/schedule.pp
@@ -5,12 +5,12 @@ define catfacts::schedule (
     $minute = '0',
     $hour = '*',
 ) {
-        include catfacts
-        include catfacts::params
-        cron {$name:
-            command => "${catfacts::params::fortune_location} /etc/catfacts/catfacts -s -n 140 | ${catfacts::params::mailer_location} -f ${sender} ${target}",
-            minute  => $minute,
-            hour    => $hour,
-            user    => $user
+    include ::catfacts
+    include ::catfacts::params
+    cron {$name:
+        command => "${catfacts::params::fortune_location} /etc/catfacts/catfacts -s -n 140 | ${catfacts::params::mailer_location} -f ${sender} ${target}",
+        minute  => $minute,
+        hour    => $hour,
+        user    => $user,
     }
 }
